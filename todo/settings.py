5436a6bc58eb4260e2fc839b5d038619274c0e63
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2025-11-24 15:15:05 +0100

    Updated tag to 1.1.0

--- todo/settings.py
+++ todo/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # App version
-APP_VERSION = "1.0.0"
+APP_VERSION = "1.1.0"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
