b6523f4c9f8b4b36c19a5a7f303a7c27e3bdfa03
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2025-12-08 15:04:21 +0100

    Added test list and decorators to tests

--- tests/tasks/test_admin.py
+++ tests/tasks/test_admin.py
@@ -2,9 +2,11 @@ from django.contrib.admin.sites import site
 from django.test import SimpleTestCase
 
 from tasks.models import Task
+from ..decorators import tc
 
 
 class AdminTests(SimpleTestCase):
+    @tc("TC008")
     def test_task_is_registered_in_admin(self):
         from tasks import admin  # noqa: F401
         self.assertIn(Task, site._registry)
