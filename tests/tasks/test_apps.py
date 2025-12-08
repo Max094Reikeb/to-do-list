31dc33f73047fba6cb86e295449c274550bf7ca7
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2025-12-08 15:13:01 +0100

    Added test list and decorators to tests

--- tests/tasks/test_apps.py
+++ tests/tasks/test_apps.py
@@ -1,8 +1,10 @@
 from django.test import SimpleTestCase
 
 from tasks.apps import TasksConfig
+from ..decorators import tc
 
 
 class TasksConfigTests(SimpleTestCase):
+    @tc("TC010")
     def test_app_config_name(self):
         self.assertEqual(TasksConfig.name, "tasks")
--- tests/tasks/test_forms.py
+++ tests/tasks/test_forms.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
 
 from tasks.forms import TaskForm
+from ..decorators import tc
 
 
 class TaskFormTests(TestCase):
+    @tc("TC015")
     def test_form_placeholder_is_set(self):
         form = TaskForm()
         self.assertIn("title", form.fields)
@@ -13,6 +15,7 @@ class TaskFormTests(TestCase):
             "Add new task",
         )
 
+    @tc("TC003")
     def test_form_is_valid_with_title(self):
         form = TaskForm(data={"title": "New task", "complete": False})
         self.assertTrue(form.is_valid())
